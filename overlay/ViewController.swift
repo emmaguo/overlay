2973e0356902c66d8e006663fd5cdec89668c963
Emma Guo <emmahongguo@gmail.com>
2016-02-23 23:27:43 -0800

    Setup Xcode project with autolayout disabled

---
+++ overlay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  overlay
+//
+//  Created by Emma Guo on 2/23/16.
+//  Copyright © 2016 overlay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
